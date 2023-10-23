b63b05705afb518e916616b9f3a8570288beb8f8
PavsTEC <pablo.venegas@estudiantec.cr>
2023-10-23 16:51:22 -0600

    nuevas reglas

--- proyecto/covid-19.pl
+++ proyecto/covid-19.pl
@@ -63,6 +63,58 @@ es_contacto_estrecho_indirecto(Persona1, Persona2) :-
 es_contacto_estrecho_indirecto(Persona1, Persona2) :-
     es_contacto_estrecho(Persona1, Persona2).
 
+% Regla para determinar si alguien fue vacunado con Pfizer
+fue_vacunado_pfizer(Persona) :-
+    pfizer(Persona).
+
+% Regla para determinar si alguien fue vacunado con Moderna
+fue_vacunado_moderna(Persona) :-
+    moderna(Persona).
+
+% Regla para determinar si alguien fue vacunado con Johnson & Johnson
+fue_vacunado_johnson(Persona) :-
+    johnson(Persona).
+
+% Regla para determinar si alguien ha recibido ambas dosis de la vacuna
+recibio_dosis_completa(Persona) :-
+    primera_dosis(Persona),
+    segunda_dosis(Persona).
+
+% Regla para determinar si alguien es elegible para la vacuna de refuerzo
+elegible_para_refuerzo(Persona) :-
+    recibio_dosis_completa(Persona),
+    paso_tiempo_suficiente_desde_ultima_dosis(Persona).
+
+% Regla para determinar si alguien tiene una condición de salud subyacente
+tiene_condicion_salud_subyacente(Persona) :-
+    tiene_diabetes(Persona);
+    tiene_enfermedad_cardiaca(Persona);
+    tiene_enfermedad_pulmonar_cronica(Persona).
+
+% Regla para determinar si alguien se ha hecho la prueba de COVID-19
+se_hizo_prueba_covid(Persona) :-
+    prueba_covid(Persona).
+
+% Regla para determinar si alguien dio positivo en la prueba de COVID-19
+dio_positivo_covid(Persona) :-
+    resultado_prueba_covid(Persona, positivo).
+
+% Regla para determinar si alguien dio negativo en la prueba de COVID-19
+dio_negativo_covid(Persona) :-
+    resultado_prueba_covid(Persona, negativo).
+
+% Regla para determinar si alguien necesita hacerse la prueba de COVID-19
+necesita_prueba_covid(Persona) :-
+    tiene_sintomas_covid(Persona);
+    es_contacto_estrecho(Persona, PersonaInfectada), dio_positivo_covid(PersonaInfectada).
+
+% Regla para determinar si alguien necesita hacerse una prueba de seguimiento de COVID-19
+necesita_prueba_seguimiento_covid(Persona) :-
+    dio_positivo_covid(Persona),
+    paso_tiempo_suficiente_desde_ultima_prueba(Persona).
+
+
+
 
 
 
