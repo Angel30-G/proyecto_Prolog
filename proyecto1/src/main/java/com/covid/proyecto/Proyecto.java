000511feb323f064ea0915c623976af99b664ae3
Angel <gabrielvargas493@gmail.com>
2023-10-25 19:16:58 -0600

    Se agregan los hechos al menu

--- proyecto1/src/main/java/com/covid/proyecto/Proyecto.java
+++ proyecto1/src/main/java/com/covid/proyecto/Proyecto.java
@@ -47,8 +47,88 @@ public class Proyecto {
             switch (opcionPrincipal) {
                 case 1:
                     // Opciones de Hechos (si las tienes)
-                    System.out.println("Opciones de Hechos (pendiente de implementación)");
+                    do {
+                        System.out.println("Elija una opción:");
+                        System.out.println("1. Ver personas infectadas");
+                        System.out.println("2. Personas enfermas");
+                        System.out.println("3. Personas contagiadas");
+                        System.out.println("4. Personas con fiebre");
+                        System.out.println("5. Personas con tos");
+                        System.out.println("6. Personas con dificultades para respirar");
+                        System.out.println("7. Personas que tienen la primera dosis");
+                        System.out.println("8. Personas que tienen la segunda dosis");
+                        System.out.println("9. Personas que han realizado la prueba del covid");
+                        System.out.println("10. Resultados de las pruebas del covid");
+                        System.out.println("11. Personas que tiene diabetes");
+                        System.out.println("12. Personas que tienen enfermedades cardiacas");
+                        System.out.println("13. Personas que tienen enfermedades pulmunares");
+                        System.out.println("14. Personas que estan en cuarentena");
+                        System.out.println("15. Personas que hacen mucho no se vacunan");
+                        System.out.println("16. Personas que hace mucho no se hacen la prueba del covid");
+                        System.out.println("0. Volver al menú principal");
+                        opcionSecundaria = scanner.nextInt();
+
+                        // Llamadas a los métodos según la opción seleccionada
+                        switch (opcionSecundaria) {
+                            case 1:
+                                hecho.infectado();
+                                break;
+                            case 2:
+                                hecho.personas_enfermas();
+                                break;
+                            case 3:
+                                hecho.personas_contagiadas();
+                                break;
+                            case 4:
+                                hecho.fiebre();
+                                break;
+                            case 5:
+                                hecho.tos();
+                                break;
+                            case 6:
+                                hecho.dificultad_respirar();
+                                break;
+                            case 7:
+                                hecho.primera_dosis();
+                                break;
+                            case 8:
+                                hecho.segunda_dosis();
+                                break;
+                            case 9:
+                                hecho.prueba_covid();
+                                break;
+                            case 10:
+                                hecho.resultado_prueba();
+                                break;
+                            case 11:
+                                hecho.tiene_diabetes();
+                                break;
+                            case 12:
+                                hecho.tiene_enfermedad_cardiaca();
+                                break;
+                            case 13:
+                                hecho.tiene_enfermedad_pulmonar_cronica();
+                                break;
+                            case 14:
+                                hecho.esta_en_cuarentena();
+                                break;
+                            case 15:
+                                hecho.paso_tiempo_suficiente_desde_ultima_dosis();
+                                break;
+                            case 16:
+                                hecho.paso_tiempo_suficiente_desde_ultima_prueba();
+                                break;
+                            case 0:
+                                System.out.println("Regresando al menu principal...");
+                                break;
+                            default:
+                                System.out.println("Opción no válida");
+                                break;
+                        }
+
+                    } while (opcionSecundaria != 0);
                     break;
+    
                 case 2:
                     // Menú de Reglas
                     do {
